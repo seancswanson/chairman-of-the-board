021acc1c378f113b755a5e0d7a95465678b13071
seanthaswan <owneruser@Owners-MBP.home>
2017-12-10 12:09:28 -0800

    Added audio assets on load to JS, added authorship info

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-console.log("JS loaded");
-// $('body').css('cursor', 'url(/path/to/image),auto');
+console.log('JS loaded');
+
+//variables for sound and document selectors
+var menuMusic = new Audio('../assets/audio/menu.wav');
+var menuNoise = new Audio('../assets/audio/office.wav');
+
+
+// menuMusic.play();
+// menuNoise.play();
+//loops menu sounds
+menuMusic.addEventListener('ended', function(){
+	this.currentTime=0;
+	this.play();
+}, false )
+menuNoise.addEventListener('ended', function(){
+	this.currentTime=0;
+	this.play();
+}, false )
+//Add office ambient, jazz sound to start screen
+
+//Add click noise to buttons
+
+//Show scenario, result, action button divs on start game function
+
