361f73a13bac234b29275f9db1601218e886fea5
seanthaswan <owneruser@Owners-MBP.home>
2017-12-10 15:33:51 -0800

    added variables for scores and document selectors

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
 console.log('JS loaded');
 
-//variables for sound and document selectors
+//variables for sound 
 var menuMusic = new Audio('../assets/audio/menu.wav');
 var menuNoise = new Audio('../assets/audio/office.wav');
+//variables for document selectors
+
+//** Starting Screen Selectors
+var startScreen = $("#start-screen");
+var chairmanTitle = $("#chairman-title");
+var playButton = $("#start");
+var instructionsButton = $("#info");
+//** Game selectors 
+var gameScreen = $("#game-div");
+var scenarioDiv = $("#scenario-div");
+var scenarioP = $("#scenario-text");
+var resultDiv = $("#result-div");
+var resultP = $("#result-text");
+var actionsDiv = $("#actions");
+var actionButtons = $(".action-button");
+var scoresDiv = $("#scores");
+var weekP = $("#week");
+var confidenceP = $("#confidence");
+var brownieP = $("#brownie-points");
+
 
 //variables for scores, week counter for promotion
+var weekCount = 0;
+var confidenceCount = 0;
+var brownieCount = 0;
+
+//Add event listeners to start game
 
 // menuMusic.play();
 // menuNoise.play();
