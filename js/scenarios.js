cb7fc1124c7a692c3601dac8c07d89f375ceff7f
seanthaswan <owneruser@Owners-MacBook-Pro.local>
2017-12-11 11:00:07 -0800

    added basic menu navigation, instructions, start/load game functionality

--- js/scenarios.js
+++ js/scenarios.js
@@ -1,5 +1,67 @@
 // objects for job position: job title > situations > choices > starting score?
 
 // var for mailroom clerk position object
+var JobPositions = [
+{
+	//**job title
+	title: "Mailroom Clerk",
+	//five scenarios, one for each week
+	scenarios: ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"],
+	//possible actions for each scenario
+	actions: ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"],
+	//outcome text for each action taken
+	outcome: ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"],
+	//score change depending on action taken.
+	score: [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--], [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--] [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--] [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--]
+}
+{
+	//**job title
+	title: "Office Intern",
+	//five scenarios, one for each week
+	scenarios: ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"],
+	//possible actions for each scenario
+	actions: ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"],
+	//outcome text for each action taken
+	outcome: ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"],
+	//score change depending on action taken.
+	score: [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--], [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--] [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--] [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--]
+}
+{
+	//**job title
+	title: "Sales Associate",
+	//five scenarios, one for each week
+	scenarios: ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"],
+	//possible actions for each scenario
+	actions: ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"],
+	//outcome text for each action taken
+	outcome: ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"],
+	//score change depending on action taken.
+	score: [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--], [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--] [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--] [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--]
+}
+{
+	//**job title
+	title: "Office Manager",
+	//five scenarios, one for each week
+	scenarios: ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"],
+	//possible actions for each scenario
+	actions: ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"],
+	//outcome text for each action taken
+	outcome: ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"],
+	//score change depending on action taken.
+	score: [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--], [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--] [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--] [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--]
+}
+{
+	//**job title
+	title: "President",
+	//five scenarios, one for each week
+	scenarios: ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"], ["0", "1", "2", "3", "4"],
+	//possible actions for each scenario
+	actions: ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"],
+	//outcome text for each action taken
+	outcome: ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"], ["0", "1", "2", "3"],
+	//score change depending on action taken.
+	score: [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--], [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--] [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--] [confidenceCount--, confidenceCount++ && brownieCount++, confidence++, confidenceCount--]
+}
+]
 
 // var for mailroom manager position object
--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ var startScreen = $("#start-screen");
 var chairmanTitle = $("#chairman-title");
 var playButton = $("#start");
 var instructionsButton = $("#info");
+var instructionDiv = $("#instructions-div");
+var exitInstructions = $("#return-to-menu");
 //** Game selectors 
 var gameScreen = $("#game-div");
 var scenarioDiv = $("#scenario-div");
@@ -22,18 +24,47 @@ var scoresDiv = $("#scores");
 var weekP = $("#week");
 var confidenceP = $("#confidence");
 var brownieP = $("#brownie-points");
-
-
+// selectors for action buttons
+var actionsDiv = $("#actions-div");
+var actionButtons = $(".action-button");
+//** individual action buttons,
+//** 1>TopLeft, 2>TopRight,
+//** 3>BottomLeft, 4>BottomRight
+var action0 = $("#action0");
+var action1 = $("#action1");
+var action2 = $("#action2");
+var action3 = $("#action3");
 //variables for scores, week counter for promotion
 var weekCount = 0;
 var confidenceCount = 0;
 var brownieCount = 0;
 
 //Add event listeners to start game
+document.addEventListener("DOMContentLoaded", loadGame);
+
+function loadGame() {
+	gameScreen.hide();
+	instructionDiv.hide();
+	playButton.on("click", startGame);
+	instructionsButton.on("click", openInstructions);
+}
+
+function startGame() {
+	startScreen.hide();
+	gameScreen.show();
+}
+
+function openInstructions(){
+	instructionDiv.show();
+	exitInstructions.on("click", function(){
+		instructionDiv.hide();
+	})
+}
+
 
 // menuMusic.play();
 // menuNoise.play();
-//loops menu sounds
+// loops menu sounds
 menuMusic.addEventListener('ended', function(){
 	this.currentTime=0;
 	this.play();
