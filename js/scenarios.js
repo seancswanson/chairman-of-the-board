36ca2e5dd64b96d7ef74de9cfd171d716e300ce0
seanthaswan <owneruser@Owners-MacBook-Pro.local>
2017-12-12 11:53:12 -0800

    Set up logic for keeping track of scores, showing results of actions, and moving on to the next scenario in that same job title

--- js/scenarios.js
+++ js/scenarios.js
@@ -1,177 +1,172 @@
 // objects for job position: job title > situations > choices > starting score?
 
 // var for mailroom clerk position object
-var jobPositions = [
-{
-	//**job title
-	title: "Mailroom Clerk",
-	scenarios: [
-		{ 
-			//scenario text
-			text: "The building you are in looks brand-spanking-new on the outside yet you discover that the mail room equipment outdates you by decades... something is wrong with the printer again and it appears to spit out a giberish error >> 'fatalError #616: unexpected Token' << . What do you do? ",
-			//what happens next?
-			outcomes: [
-				{ 
-					//action, result text, score counters
-					action: ">Search online to debug and hope their is a 'Corporate Overflow' answer, then follow the best voted answer.", 
-					result: "Your stack skills are impressive! You find out you need to power cycle the printer. Nice work!",
-					confidence: 1,
-					brownie: 0
-				},
-				{ 
-					action: ">Submit a ticket to maintenance hoping they fix it.", 
-					result: "Maintenance never responds and you can't turn in your daily mail summary. Bummer.",
-					confidence: -1,
-					brownie: -1
-				},
-								{ 
-					action: ">When noone is looking you smack the side of the printer and will take full credit for the 'repair' (if it works).", 
-					result: "The printer never errors again and your manager praises you for your ingenuity! They think you might have some worth after all.",
-					confidence: +2,
-					brownie: 1
-				},
-								{ 
-					action: ">Open up the maintenance door of the damned thing and investigate.", 
-					result: "You find a banana peel in the rollers and remove it... but then slice your finger on the metal components whodunit??",
-					confidence: -1,
-					brownie: 0
-				}]
-// 		{ 
-// 			//scenario text
-// 			text: "The building you are in looks brand-spanking-new on the outside yet you discover that the mail room equipment outdates you by decades... something is wrong with the printer again and it appears to spit out a giberish error >> 'fatalError #616: unexpected Token' << . What do you do? ",
-// 				outcomes: [
-// 				{ 
-// 					action: ">Search online to debug and hope their is a 'Corporate Overflow' answer, then follow the best voted answer.", 
-// 					result: "Your stack skills are impressive! You find out you need to power cycle the printer. Nice work!",
-// 					confidence: 1,
-// 					brownie: 0
-// 				},
-// 				{ 
-// 					action: ">Submit a ticket to maintenance hoping they fix it.", 
-// 					result: "Maintenance never responds and you can't turn in your daily mail summary. Bummer.",
-// 					confidence: -1,
-// 					brownie: -1
-// 				},
-// 								{ 
-// 					action: ">When noone is looking you smack the side of the printer and will take full credit for the 'repair' (if it works).", 
-// 					result: "The printer never errors again and your manager praises you for your ingenuity! They think you might have some worth after all.",
-// 					confidence: +2,
-// 					brownie: 1
-// 				},
-// 								{ 
-// 					action: ">Open up the maintenance door of the damned thing and investigate.", 
-// 					result: "You find a banana peel in the rollers and remove it... but then slice your finger on the metal components whodunit??",
-// 					confidence: -1,
-// 					brownie: 0
-// 				},
-// 			]
-// 		},
-// 	],
-// },
-// {
-// 	//**job title
-// 	title: "Office Intern",
-// 	scenarios: [
-// 		{ 
-// 			//scenario text
-// 			text: "Printer is jammed.",
-// 			//what happens next?
-// 			outcomes: [
-// 				{ 
-// 					//action, result text, score counters
-// 					action: "Unjam printer", 
-// 					result: "Get promoted",
-// 					confidence: 1,
-// 					brownie: 1
-// 				},
-// 				{ 
-// 					action: "Ignore printer", 
-// 					result: "Nothing happens. Bob gets promoted instead.",
-// 					confidence: -1,
-// 					brownie: 0
-// 				}
-// 			]
-// 		},
-// 	],
-// },
-// {
-// 	//**job title
-// 	title: "Sales Associate",
-// 	scenarios: [
-// 		{ 
-// 			//scenario text
-// 			text: "Printer is jammed.",
-// 			//what happens next?
-// 			outcomes: [
-// 				{ 
-// 					//action, result text, score counters
-// 					action: "Unjam printer", 
-// 					result: "Get promoted",
-// 					confidence: 1,
-// 					brownie: 1
-// 				},
-// 				{ 
-// 					action: "Ignore printer", 
-// 					result: "Nothing happens. Bob gets promoted instead.",
-// 					confidence: -1,
-// 					brownie: 0
-// 				}
-// 			]
-// 		},
-// 	],
-// },
-// {
-// 	//**job title
-// 	title: "Floor Manager",
-// 	scenarios: [
-// 		{ 
-// 			//scenario text
-// 			text: "Printer is jammed.",
-// 			//what happens next?
-// 			outcomes: [
-// 				{ 
-// 					//action, result text, score counters
-// 					action: "Unjam printer", 
-// 					result: "Get promoted",
-// 					confidence: 1,
-// 					brownie: 1
-// 				},
-// 				{ 
-// 					action: "Ignore printer", 
-// 					result: "Nothing happens. Bob gets promoted instead.",
-// 					confidence: -1,
-// 					brownie: 0
-// 				}
-// 			]
-// 		},
-// 	],
-// },
-// {
-// 	//**job title
-// 	title: "President",
-// 	scenarios: [
-// 		{ 
-// 			//scenario text
-// 			text: "Printer is jammed.",
-// 			//what happens next?
-// 			outcomes: [
-// 				{ 
-// 					//action, result text, score counters
-// 					action: "Unjam printer", 
-// 					result: "Get promoted",
-// 					confidence: 1,
-// 					brownie: 1
-// 				},
-// 				{ 
-// 					action: "Ignore printer", 
-// 					result: "Nothing happens. Bob gets promoted instead.",
-// 					confidence: -1,
-// 					brownie: 0
-// 				}
-// 			]
-// 		},
-// 	],
-}]
-}]
+var jobPositions = [{
+            //**job title
+            title: "Mailroom Clerk",
+            scenarios: [{
+                    //scenario text
+                    text: "The building you are in looks brand-spanking-new on the outside yet you discover that the mail room equipment outdates you by decades... something is wrong with the printer again and it appears to spit out a giberish error >> 'fatalError #616: unexpected Token' << . What do you do? ",
+                    //what happens next?
+                    outcomes: [{
+                            //action, result text, score counters
+                            action: ">Search online to debug and hope their is a 'Corporate Overflow' answer, then follow the best voted answer.",
+                            result: "Your stack skills are impressive! You find out you need to power cycle the printer. Nice work!",
+                            confidence: 1,
+                            brownie: 0
+                        },
+                        {
+                            action: ">Submit a ticket to maintenance hoping they fix it.",
+                            result: "Maintenance never responds and you can't turn in your daily mail summary. And your manager chews you out. Bummer.",
+                            confidence: -1,
+                            brownie: -1
+                        },
+                        {
+                            action: ">When noone is looking you smack the side of the printer and will take full credit for the 'repair' (if it works).",
+                            result: "The printer never errors again and your manager praises you for your ingenuity! They think you might have some worth after all.",
+                            confidence: 2,
+                            brownie: 1
+                        },
+                        {
+                            action: ">Open up the maintenance door of the damned thing and investigate.",
+                            result: "You find a banana peel in the rollers and remove it... but then slice your finger on the metal components whodunit??",
+                            confidence: -1,
+                            brownie: 0
+                        }
+                    ]
+                },
+                {
+                    //scenario text
+                    text: "While in a hurry to filter the corporations 'junk' mail, you accidently drop your stack of envelopes and see that one of them is addressed to the Chairman with confidential label and text that reads 'Sensitive stakeholder info'... you remember you have stock in the company and that this information could affect you. What do you do?",
+                    outcomes: [{
+                            action: ">This seems urgent! You head upstairs and personally deliver the letter to the Chairman.",
+                            result: "You are stopped by the Chairman's secretary. She takes a look at the letter and appreciates the gesture but wonders why you didn't send it through the tube. Nice try.",
+                            confidence: -1,
+                            brownie: 0
+                        },
+                        {
+                            action: ">No one will notice it's missing, things get lost in the mailroom. You open it and read the information for yourself.",
+                            result: "JACKPOT!!! DING DING DING! The company is working on a revolutionary product and the value is about to skyrocket in a week. Time to sell! The Chairman apparently recieved a personal email with the same information.",
+                            confidence: 2,
+                            brownie: 0
+                        },
+                        {
+                            action: ">No one will notice it's missing, things get lost in the mailroom. You open it and read the information for yourself.",
+                            result: "Super sad day... it appears the company is about to take a huge nosedive financially after the President publically made a racist comment about the Shawarma truck owner outside of the tower... goodbye retirement! The manager sees you crying and finds out you snooped.",
+                            confidence: -1,
+                            brownie: -1
+                        },
+                        {
+                            action: ">This looks different than other letters you have seen so you pass it along to the mail room manager.",
+                            result: "It seems you just passed along a very important baton and the manager get's recognized for acting so quickly on 'their' discovery. But not without graciously thanking you.",
+                            confidence: 1,
+                            brownie: 1
+                        }
+                    ]
+                }]
+            }];
+        // {
+        // 	//**job title
+        // 	title: "Office Intern",
+        // 	scenarios: [
+        // 		{ 
+        // 			//scenario text
+        // 			text: "Printer is jammed.",
+        // 			//what happens next?
+        // 			outcomes: [
+        // 				{ 
+        // 					//action, result text, score counters
+        // 					action: "Unjam printer", 
+        // 					result: "Get promoted",
+        // 					confidence: 1,
+        // 					brownie: 1
+        // 				},
+        // 				{ 
+        // 					action: "Ignore printer", 
+        // 					result: "Nothing happens. Bob gets promoted instead.",
+        // 					confidence: -1,
+        // 					brownie: 0
+        // 				}
+        // 			]
+        // 		},
+        // 	],
+        // },
+        // {
+        // 	//**job title
+        // 	title: "Sales Associate",
+        // 	scenarios: [
+        // 		{ 
+        // 			//scenario text
+        // 			text: "Printer is jammed.",
+        // 			//what happens next?
+        // 			outcomes: [
+        // 				{ 
+        // 					//action, result text, score counters
+        // 					action: "Unjam printer", 
+        // 					result: "Get promoted",
+        // 					confidence: 1,
+        // 					brownie: 1
+        // 				},
+        // 				{ 
+        // 					action: "Ignore printer", 
+        // 					result: "Nothing happens. Bob gets promoted instead.",
+        // 					confidence: -1,
+        // 					brownie: 0
+        // 				}
+        // 			]
+        // 		},
+        // 	],
+        // },
+        // {
+        // 	//**job title
+        // 	title: "Floor Manager",
+        // 	scenarios: [
+        // 		{ 
+        // 			//scenario text
+        // 			text: "Printer is jammed.",
+        // 			//what happens next?
+        // 			outcomes: [
+        // 				{ 
+        // 					//action, result text, score counters
+        // 					action: "Unjam printer", 
+        // 					result: "Get promoted",
+        // 					confidence: 1,
+        // 					brownie: 1
+        // 				},
+        // 				{ 
+        // 					action: "Ignore printer", 
+        // 					result: "Nothing happens. Bob gets promoted instead.",
+        // 					confidence: -1,
+        // 					brownie: 0
+        // 				}
+        // 			]
+        // 		},
+        // 	],
+        // },
+        // {
+        // 	//**job title
+        // 	title: "President",
+        // 	scenarios: [
+        // 		{ 
+        // 			//scenario text
+        // 			text: "Printer is jammed.",
+        // 			//what happens next?
+        // 			outcomes: [
+        // 				{ 
+        // 					//action, result text, score counters
+        // 					action: "Unjam printer", 
+        // 					result: "Get promoted",
+        // 					confidence: 1,
+        // 					brownie: 1
+        // 				},
+        // 				{ 
+        // 					action: "Ignore printer", 
+        // 					result: "Nothing happens. Bob gets promoted instead.",
+        // 					confidence: -1,
+        // 					brownie: 0
+        // 				}
+        // 			]
+        // 		},
+        // 	],
 
-// var for mailroom manager position object
+        // var for mailroom manager position object
--- js/script.js
+++ js/script.js
@@ -21,83 +21,133 @@ var resultP = $("#result-text");
 var actionsDiv = $("#actions");
 var actionButtons = $(".action-button");
 var scoresDiv = $("#scores");
-var weekP = $("#week");
-var confidenceP = $("#confidence");
-var brownieP = $("#brownie-points");
+var weekLi = $("#week");
+var confidenceLi = $("#confidence");
+var brownieLi = $("#brownie-points");
 // selectors for action buttons
 var actionsDiv = $("#actions-div");
-//** individual action buttons,
-//** 1>TopLeft, 2>TopRight,
-//** 3>BottomLeft, 4>BottomRight
-var action0 = $("#action0");
-var action1 = $("#action1");
-var action2 = $("#action2");
-var action3 = $("#action3");
 //variables for scores, week counter for promotion
 var weekCount = 0;
 var confidenceCount = 0;
 var brownieCount = 0;
+// Starts job position at mail clerk.
+var jobIndex = 0;
+// For referencing scenario
+var sceneIndex = 0;
+// For referencing choice made
+var actionIndex;
+
+// For button for next scene 
+var nextScene = $('<button class="action-button" id="nextButton">Next scene</button>');
 
 //Add event listeners to start game
 document.addEventListener("DOMContentLoaded", loadGame);
 
 function loadGame() {
-	gameScreen.hide();
-	instructionDiv.hide();
-	playButton.on("click", startGame);
-	instructionsButton.on("click", openInstructions);
+    gameScreen.hide();
+    instructionDiv.hide();
+    playButton.on("click", startGame);
+    instructionsButton.on("click", openInstructions);
 }
 
 function startGame() {
-	startScreen.hide();
-	gameScreen.show();
-	mailRoomClerk();
+    startScreen.hide();
+    gameScreen.show();
+    mailRoomClerk(sceneIndex);
 }
 
-function openInstructions(){
-	instructionDiv.show();
-	exitInstructions.on("click", function(){
-		instructionDiv.hide();
-	})
+function openInstructions() {
+    instructionDiv.show();
+    exitInstructions.on("click", function() {
+        instructionDiv.hide();
+    })
 }
 
 //grab random scenario from first obect array
-function mailRoomClerk(){
-	// var scenario = Math.floor(Math.random() * jobPositions[0].scenarios.length)-1;
-	// grabs the text from a scenario nested in the current job title object
-	$(scenarioP).text(jobPositions[0].scenarios[0].text);
-	// for each possible choice, make a button for it.
-	for(var i = 0; i<jobPositions[0].scenarios[0].outcomes.length; i++){
-		console.log(i);
-		var choice = $('<button class=action-button>'+jobPositions[0].scenarios[0].outcomes[i].action+'</button>');
-	// add tht button to the actionsDiv
-	$(actionsDiv).append(choice);
-	};
-	var actionButtons = $(".action-button");
-	// on action click I want to return the index of that button and then 
-	// populate the correlated outcome text by calling it with the action index... 
-	$(actionButtons).on("click", function(){
-		$(this).on("click", function(){
-					actionIndex = $(this).index(actionButtons);
-					console.log(actionIndex);
-		})
-	})
+function mailRoomClerk(sceneIndex) {
+    // var scenario = Math.floor(Math.random() * jobPositions[0].scenarios.length)-1;
+    // grabs the text from a scenario nested in the current job title object
+    $(scenarioP).text(jobPositions[0].scenarios[sceneIndex].text);
+    console.log(jobPositions[0].scenarios[sceneIndex].text);
+    // for each possible choice, make a button for it.
+    for (var i = 0; i < jobPositions[0].scenarios[sceneIndex].outcomes.length; i++) {
+        console.log(i);
+        var choice = $('<button class=action-button>' + jobPositions[0].scenarios[sceneIndex].outcomes[i].action + '</button>');
+        // add tht button to the actionsDiv
+        $(actionsDiv).append(choice);
+    };
+    var actionButtons = $(".action-button");
+    // on action click I want to return the index of that button and then 
+    // populate the correlated outcome text by calling it with the button 
+    $(actionButtons).on("click", function() {
+        actionIndex = $(actionButtons).index(this);
+        takeAction();
+    });
+}
+
+
+// After any action is taken, displays the result
+function takeAction() {
+    console.log(actionIndex);
+    actionsDiv.html("");
+    var result = $('<p id="result-text">' + jobPositions[0].scenarios[sceneIndex].outcomes[actionIndex].result + '</p>');
+    $(resultDiv).append(result)
+    var confidence = jobPositions[0].scenarios[sceneIndex].outcomes[actionIndex].confidence;
+    var confidenceMessage = $('<p class="inline-score" id="result-text">' + 'Confidence +' + confidence + '</p>');
+    var brownie = jobPositions[0].scenarios[sceneIndex].outcomes[actionIndex].brownie;
+    var brownieMessage = $('<p class="inline-score" id="result-text">' + 'Brownie Points +' + jobPositions[0].scenarios[sceneIndex].outcomes[actionIndex].brownie + '</p>');
+    if (jobPositions[0].scenarios[sceneIndex].outcomes[actionIndex].confidence >= 0 && jobPositions[0].scenarios[sceneIndex].outcomes[actionIndex].brownie > 0) {
+        console.log("1st if");
+        $(resultDiv).append(confidenceMessage).append(brownieMessage);
+        updateScores(confidence, brownie);
+    } else if (jobPositions[0].scenarios[sceneIndex].outcomes[actionIndex].confidence >= 0) {
+        console.log("1st else if");
+        $(resultDiv).append(confidenceMessage);
+        updateScores(confidence, brownie);
+    } else if (jobPositions[0].scenarios[sceneIndex].outcomes[actionIndex].confidence < 0 && jobPositions[0].scenarios[sceneIndex].outcomes[actionIndex].brownie < 0) {
+        console.log("2nd else if");
+        confidenceMessage = $('<p class="inline-score" id="result-text">' + 'Confidence ' + jobPositions[0].scenarios[sceneIndex].outcomes[actionIndex].confidence + '</p>');
+        brownieMessage = $('<p class="inline-score" id="result-text">' + 'Brownie Points ' + jobPositions[0].scenarios[sceneIndex].outcomes[actionIndex].brownie + '</p>');
+        $(resultDiv).append(confidenceMessage).append(brownieMessage);
+        updateScores(confidence, brownie);
+    } else {
+        console.log("else");
+        confidenceMessage = $('<p class="inline-score" id="result-text">' + 'Confidence ' + jobPositions[0].scenarios[sceneIndex].outcomes[actionIndex].confidence + '</p>');
+        $(resultDiv).append(confidenceMessage);
+        updateScores(confidence, brownie);
+    };
+    actionsDiv.append(nextScene);
+    sceneIndex++;
+    nextScene.on("click", function(){
+    	resultDiv.html("");
+    	this.remove();
+    	mailRoomClerk(sceneIndex);
+    });
+};
+
+// Updates week, confidence, brownie points score
+function updateScores(confidence, brownie){
+	var newConfidence = confidenceCount + confidence;
+	var newBrownie = brownieCount + brownie;
+	weekCount++;
+	$(weekLi).text("Week: " + weekCount) 
+	$(confidenceLi).text("Confidence: " + newConfidence);
+	$(brownieLi).text("Brownie Points: " + newBrownie);
 }
 
 // menuMusic.play();
 // menuNoise.play();
 // loops menu sounds
-menuMusic.addEventListener('ended', function(){
-	this.currentTime=0;
-	this.play();
-}, false )
-menuNoise.addEventListener('ended', function(){
-	this.currentTime=0;
-	this.play();
-}, false )
+menuMusic.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false)
+menuNoise.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false)
 //Add office ambient, jazz sound to start screen
 
 //Add click noise to buttons
 
 //Show scenario, result, action button divs on start game function
-
